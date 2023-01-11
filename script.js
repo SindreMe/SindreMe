53aa647d8df9a2dc08410e3eab03b1fcdc03d184
SindreMe <122259979+SindreMe@users.noreply.github.com>
2023-01-11 15:07:39 +0100

    dag 3

---
+++ script.js
@@ -0,0 +1,17 @@
+var redmode = false
+    function endrebakgrunn() {
+        if (redmode == false) {
+            document.getElementById('background').style.backgroundColor = 'black'
+            document.getElementById('text').style.color = 'red'
+            document.getElementById("button-change-color").style.backgroundColor = "red"
+            redmode = true
+        }
+        else {
+            document.getElementById('background').style.backgroundColor = 'white'
+            document.getElementById('text').style.color = 'black'
+            document.getElementById("button-change-color").style.backgroundColor = "white"
+            redmode = false
+        }
+
+    }
+    
