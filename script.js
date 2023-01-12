16460157c4da9c7b7b835e52b7d34f0f236db01f
SindreMe <122259979+SindreMe@users.noreply.github.com>
2023-01-12 13:23:49 +0100

    bugfix3

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ var redmode = false
     function endrebakgrunn() {
         if (redmode == false) {
             document.getElementById('background').style.backgroundColor = 'black'
+            document.getElementById('background').style.transistion = '2s'
             document.getElementById('text').style.color = 'red'
             document.getElementById("button-change-color").style.backgroundColor = "red"
             redmode = true
         }
         else {
             document.getElementById('background').style.backgroundColor = 'white'
+            document.getElementById('background').style.transistion = '2s'
             document.getElementById('text').style.color = 'black'
             document.getElementById("button-change-color").style.backgroundColor = "white"
             redmode = false
         }
 
     }
+    
+    //darkmode mode function
     
